2d1a06de7840c09c843c8025933b31916ca3e9ab
rashidamuda <rashid.amuda@gmail.com>
2019-09-23 10:19:06 +0300

    edit js

--- js/akan.js
+++ js/akan.js
@@ -1,7 +1,6 @@
 var maleNames = ["Kwasi", "Kwandwo", "Kabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
 function giveAkanName(){
 var year = parseInt(document.getElementById("year").value);
 var month = parseInt(document.getElementById("month").value);
