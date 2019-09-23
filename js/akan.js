1dc58198d08b4aafb7759c09f067c6669b9bc57a
rashidamuda <rashid.amuda@gmail.com>
2019-09-23 08:29:45 +0300

    added gender function

--- js/akan.js
+++ js/akan.js
@@ -1,7 +1,9 @@
 var maleNames = ["Kwasi", "Kwandwo", "Kabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+var year = parseInt(document.getElementById("year").value);
+var month = parseInt(document.getElementById("month").value);
+var day = parseInt(document.getElementById("day").value);
 function giveAkanName
 var date = new Date(year +"/" +month +"/"+day);
 var dayBorn = date.getDay();
@@ -13,4 +15,8 @@ else{
     akanName = femaleNames[dayBorn];
 }
 alert("You were born on "+dayOfTheWeek[dayBorn]+ "Your name is.."+akanName);
-
+function getGender()
+var gender = document.getElementById("gender");
+for(i=0; i<gender.length; i++) {
+    if (gender[i]()
+}
