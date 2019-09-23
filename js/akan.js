d22b1e9b95fbbb7a2b0566e8420da74acb9ea81a
rashidamuda <rashid.amuda@gmail.com>
2019-09-23 10:14:09 +0300

    add functionalityto the application

--- js/akan.js
+++ js/akan.js
@@ -1,26 +1,29 @@
 var maleNames = ["Kwasi", "Kwandwo", "Kabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function giveAkanName(){
 var year = parseInt(document.getElementById("year").value);
 var month = parseInt(document.getElementById("month").value);
 var day = parseInt(document.getElementById("day").value);
 var gender = getGender();
-function giveAkanName(){
 var date = new Date(year +"/" +month +"/"+day);
 var dayBorn = date.getDay();
 var akanName;
 if(gender ==="male"){
     akanName = maleNames[dayBorn];
-}
+    }
 else{
     akanName = femaleNames[dayBorn];
-}
-alert("You were born on "+dayOfTheWeek[dayBorn]+ "Your name is.."+akanName);
-}
-function getGender()
-var gender = document.getElementById("gender");
-for(i=0; i<gender.length; i++) {
-    if (gender[i].checked)
-    return (gender [i].value);
+    }
+alert("You were born on "+dayOfTheWeek[dayBorn]+ " Your name is.."+akanName);
 }
 
+function getGender(){
+var gender = document.getElementsByName("gender");
+for(i=0; i<gender.length; i++) {
+    if (gender[i].checked){
+    return gender[i].value;
+    }
+}
+}
